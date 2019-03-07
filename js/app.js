9ab2420ce74672791f87b13ac539181c1320d810
Mehedi Hasan <mehedihasansabbirmi@gmail.com>
2019-03-08 00:09:56 +0600

    Change

--- js/app.js
+++ js/app.js
@@ -1,1 +1,237 @@
-!function(e){var t={};function n(o){if(t[o])return t[o].exports;var s=t[o]={i:o,l:!1,exports:{}};return e[o].call(s.exports,s,s.exports,n),s.l=!0,s.exports}n.m=e,n.c=t,n.d=function(e,t,o){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:o})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var o=Object.create(null);if(n.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var s in e)n.d(o,s,function(t){return e[t]}.bind(null,s));return o},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="/",n(n.s=0)}([function(e,t,n){n(1),e.exports=n(2)},function(e,t){function n(e,t){for(var n=0;n<t.length;n++){var o=t[n];o.enumerable=o.enumerable||!1,o.configurable=!0,"value"in o&&(o.writable=!0),Object.defineProperty(e,o.key,o)}}var o,s,r,u;(new(function(){function e(){!function(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}(this,e),this.classNames=["zero","one","two","three","four","five","six","seven","eight","nine"],this.secondOne=document.getElementById("second-one"),this.secondTwo=document.getElementById("second-two"),this.minuteOne=document.getElementById("minute-one"),this.minuteTwo=document.getElementById("minute-two"),this.hourOne=document.getElementById("hour-one"),this.hourTwo=document.getElementById("hour-two")}var t,i,a;return t=e,(i=[{key:"run",value:function(){var e=this;setInterval(function(){o=new Date,s=o.getSeconds()<10?[0,o.getSeconds()]:o.getSeconds().toString().split(""),r=o.getMinutes()<10?[0,o.getMinutes()]:o.getMinutes().toString().split(""),u=o.getHours()<10?[0,o.getHours()]:o.getHours().toString().split(""),e.secondOne.className="segments "+e.getClassName(s[0]),e.secondTwo.className="segments "+e.getClassName(s[1]),e.minuteOne.className="segments "+e.getClassName(r[0]),e.minuteTwo.className="segments "+e.getClassName(r[1]),e.hourOne.className="segments "+e.getClassName(u[0]),e.hourTwo.className="segments "+e.getClassName(u[1])},1e3)}},{key:"getClassName",value:function(e){return this.classNames[e]}}])&&n(t.prototype,i),a&&n(t,a),e}())).run()},function(e,t){}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/js/app.js":
+/*!***********************!*\
+  !*** ./src/js/app.js ***!
+  \***********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var Clock =
+/*#__PURE__*/
+function () {
+  function Clock() {
+    _classCallCheck(this, Clock);
+
+    this.classNames = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    this.secondSelector = document.querySelectorAll('.seconds');
+    this.minuteSelector = document.querySelectorAll('.minutes');
+    this.hourSelector = document.querySelectorAll('.hours');
+    this.dots = document.querySelectorAll('.dots *');
+    this.second = [];
+    this.minute = [];
+    this.hour = [];
+    this.date = new Date();
+    this.audio = document.getElementById('audio');
+  }
+
+  _createClass(Clock, [{
+    key: "run",
+    value: function run() {
+      var _this = this;
+
+      setInterval(function () {
+        _this.date = new Date();
+
+        _this.updateSecond();
+
+        _this.playAudio();
+      }, 1000);
+    }
+  }, {
+    key: "updateSecond",
+    value: function updateSecond() {
+      var _this2 = this;
+
+      var second = this.date.getSeconds();
+      this.second = second < 10 ? [0, second] : second.toString().split('');
+      this.secondSelector.forEach(function (secondNode, index) {
+        secondNode.className = 'segments seconds ' + _this2.getClassName(_this2.second[index]);
+      });
+
+      if (second === 0) {
+        this.updateMinute();
+      }
+    }
+  }, {
+    key: "updateMinute",
+    value: function updateMinute() {
+      var _this3 = this;
+
+      var minute = this.date.getMinutes();
+      this.minute = minute < 10 ? [0, minute] : minute.toString().split('');
+      this.minuteSelector.forEach(function (minuteNode, index) {
+        minuteNode.className = 'segments minutes ' + _this3.getClassName(_this3.minute[index]);
+      });
+
+      if (minute === 0) {
+        this.updateHour();
+      }
+    }
+  }, {
+    key: "updateHour",
+    value: function updateHour() {
+      var _this4 = this;
+
+      var hour = this.date.getHours();
+      this.hour = hour < 10 ? [0, hour] : hour.toString().split('');
+      this.hourSelector.forEach(function (hourNode, index) {
+        hourNode.className = 'segments hours ' + _this4.getClassName(_this4.hour[index]);
+      });
+    }
+  }, {
+    key: "getClassName",
+    value: function getClassName(number) {
+      return this.classNames[number];
+    }
+  }, {
+    key: "toggleDots",
+    value: function toggleDots() {
+      var _this5 = this;
+
+      setInterval(function () {
+        _this5.dots.forEach(function (dot) {
+          dot.classList.toggle('active');
+        });
+      }, 500);
+      return this;
+    }
+  }, {
+    key: "boot",
+    value: function boot() {
+      this.updateSecond();
+      this.updateMinute();
+      this.updateHour();
+      return this;
+    }
+  }, {
+    key: "playAudio",
+    value: function playAudio() {
+      this.audio.play();
+    }
+  }]);
+
+  return Clock;
+}();
+
+new Clock().boot().toggleDots().run();
+
+/***/ }),
+
+/***/ "./src/sass/app.scss":
+/*!***************************!*\
+  !*** ./src/sass/app.scss ***!
+  \***************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ 0:
+/*!*************************************************!*\
+  !*** multi ./src/js/app.js ./src/sass/app.scss ***!
+  \*************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! /media/mehedi/New Project/Projects/digital-clock/src/js/app.js */"./src/js/app.js");
+module.exports = __webpack_require__(/*! /media/mehedi/New Project/Projects/digital-clock/src/sass/app.scss */"./src/sass/app.scss");
+
+
+/***/ })
+
+/******/ });
